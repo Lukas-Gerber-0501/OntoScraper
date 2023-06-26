7f9ae847ce2d5398c69960d81eade6fa204a4ff9
Lukas-Gerber <lukas.gerber@live.at>
2023-06-26 20:13:42 +0200

    added realtionship and constant names

--- generics/generics.py
+++ generics/generics.py
@@ -1,11 +1,13 @@
 # Constants
 WEBPAGE = "webpage"
 ARTICLE = "article"
+PDF = ".pdf"
 
 # Relationships
 MATCHING_TITLE = "matching_title"
 SIBLING_OF = "sibling_of"
 PARENT_OF = "parent_of"
+MENTIONED_IN = "mentioned_in"
 NO_TITLE = "No title"
 ARTICLE_ID = "a"
 DATA_ID = "d"
